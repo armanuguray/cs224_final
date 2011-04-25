644d4d4e27a2322d14a03ea6b1d8b23cd5b3b97d
Arman Uguray <arman.uguray@gmail.com>
2011-04-25 00:41:56 -0400

    made skybox better

--- CS224-Final/SkyRenderer.cpp
+++ CS224-Final/SkyRenderer.cpp
@@ -50,14 +50,19 @@ SkyRenderer::SkyRenderer()
 
 void SkyRenderer::renderSkyBox(Camera *camera)
 {
+    // The sky box should appear to be infinitely far away, so don't render it to the depth buffer,
+    // so that it doesn't occlude anything in the scene
+    glDisable(GL_DEPTH_TEST);
     glMatrixMode(GL_MODELVIEW);
     glPushMatrix();
     glLoadIdentity();
     Vector4 center = camera->getLook(), up = camera->getUp();
+    // To give the impression of infinity, make the skybox seem to "move with the camera"
     gluLookAt(0.0, 0.0, 0.0,
               center.x, center.y, center.z,
               up.x, up.y, up.z);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // TODO: remove this line later
     glCallList(skybox);
     glPopMatrix();
+    glEnable(GL_DEPTH_TEST);
 }
