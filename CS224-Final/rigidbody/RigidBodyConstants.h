92927350290aeb5650f92bc9f5d57718fcbdc1a9
Arman Uguray <arman.uguray@gmail.com>
2011-05-09 18:09:31 -0400

    adjusted constants

--- CS224-Final/rigidbody/RigidBodyConstants.h
+++ CS224-Final/rigidbody/RigidBodyConstants.h
@@ -8,7 +8,7 @@
 // The maximum number of rigid bodies to pre-allocate for the entire simulation
 const unsigned RIGIDBODY_MAX_COUNT = 1000;
 // internal number of substep for each step of the physics simulation
-const int MAX_SUBSTEPS = 10;
+const int MAX_SUBSTEPS = 5;
 // resolution of the buoyancy image. this should be relatively low for performance reasons
 const unsigned BUOYANCY_IMAGE_RESOLUTION = 16;
 // impulse scale factor shooting boxes
@@ -18,7 +18,7 @@ const float WATER_DENSITY = 1000.0f;
 // clip height (unused)
 const float MAX_ABS_HEIGHT = 1000.0f;
 // mass of a crate (kg)
-const float BOX_MASS = 6000.f;
+const float BOX_MASS = 4900.f;
 // gravitational acceleration (m/s^2)
 const float GRAVITY = 9.8f;
 // length of one side of an object bounding box
@@ -26,9 +26,9 @@ const float SIDE_LENGTH = 2.0f;
 // per object orthgonal view extent
 const float OBJ_EXTENT = sqrt((SIDE_LENGTH*SIDE_LENGTH)+(SIDE_LENGTH*SIDE_LENGTH)) + 0.5;
 // lift and drag coefficients
-const float LIFT_COEFFICIENT = 1.3;
+const float LIFT_COEFFICIENT = 2.1;
 const float DRAG_COEFFICIENT = 2.1;
 // epsilon value that defines the dependency of face area on direction of motion. ([0, 1])
-const float LD_EPSILON = 0.3;
+const float LD_EPSILON = 0.5;
 
 #endif // RIGIDBODYCONSTANTS_H
